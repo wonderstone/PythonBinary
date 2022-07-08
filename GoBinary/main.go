ac2553bba655774fa0339887254f9a5adb55c163
wonderstone <tieqiw@hotmail.com>
2022-07-08 19:37:50 +0800

    test temp

--- GoBinary/main.go
+++ GoBinary/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"C"
+	"fmt"
+	"log"
+	"time"
+)
+
+//export helloWorld
+func helloWorld() {
+	log.Println("Hello World")
+	// iter for 10 seconds
+	for i := 0; i < 10; i++ {
+		// convert int to string
+		fmt.Println(i)
+
+		// sleep for 1 second
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Hello, World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
 }
--- main.py
+++ main.py
@@ -1,64 +1,38 @@
 # use os.system() commands.getstatusoutput() and popen() to run binarys
 
-from cgi import print_arguments
 import os
-
-# os.system() command to run binary
-res = os.system("./GoBinary/main")
-print(res)  
-
+from time import sleep
+from observable import *
 import subprocess
+import ctypes
 
-if os.path.exists("./GoBinary/main"):
-    rs,out = subprocess.getstatusoutput("./GoBinary/main")
-    print(rs)
-    print(out)
-else:
-    print("binary not found")
+library = ctypes.cdll.LoadLibrary("./GoBinary/library.so")
 
+hello_world = library.helloWorld
+hello_world()
+###############################################################################3
+# os.system() command to run binary
+# res = os.system("./GoBinary/main")
+# print(res)  
+#########################
+# subprocess.Popen() command to run binary
+# if os.path.exists("./GoBinary/main"):
+#     rs,out = subprocess.getstatusoutput("./GoBinary/main")
+#     # print(rs)
+#     print(out)
+# else:
+#     print("binary not found")
 
+#########################
 # os.popen() command to run binary
-p = os.popen("./GoBinary/main")
-print(p.read())
-p.close()
-
-
-class GlobalWealth(object):
-    def __init__(self):
-        self._global_wealth = 10.0
-        self._observers = []
-
-    @property
-    def global_wealth(self):
-        return self._global_wealth
-
-    @global_wealth.setter
-    def global_wealth(self, value):
-        self._global_wealth = value
-        for callback in self._observers:
-            print('announcing change')
-            callback(self._global_wealth)
-
-    def bind_to(self, callback):
-        print('bound')
-        self._observers.append(callback)
-
-
-class Person(object):
-    def __init__(self, data):
-        self.wealth = 1.0
-        self.data = data
-        self.data.bind_to(self.update_how_happy)
-        self.happiness = self.wealth / self.data.global_wealth
-
-    def update_how_happy(self, global_wealth):
-        self.happiness = self.wealth / global_wealth
-
+# p = os.popen("./GoBinary/main")
+# print(p.read())
+# p.close()
 
 
 if __name__ == '__main__':
-    data = GlobalWealth()
-    p = Person(data)
-    print(p.happiness)
-    data.global_wealth = 1.0
-    print(p.happiness)
+
+    pass
+
+
+
